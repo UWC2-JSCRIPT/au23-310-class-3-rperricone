e003c511554300171980145162fc506e2d4127d2
Robin <robinp31@uw.eduL>
2023-10-27 18:04:31 -0600

    Exercise 3 development branch

--- itemizedReceipt.js
+++ itemizedReceipt.js
@@ -2,7 +2,14 @@
 // with these properties: {descr, price}
 // i.e. {descr: 'Coke', price: 1.99}
 // function should log each item to the console and log a total price
-
+function logReceipt (...items){
+  let total = 0
+  items.forEach((item)=> {
+    total += item.price
+    console.log(item.descr, item.price)
+  })
+console.log(`total: ${total}`)
+}
 
 
 // Check
